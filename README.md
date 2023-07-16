26dcfe212a4d3828d932e8845a20cfd4d0b18a8a
Motasem-Sulaiman <motasemismael0@gmail.com>
2023-07-16 10:33:42 +0000

    edit readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # aws-server
+
+
+![](./Screenshot%202023-07-16%20133026.png)
+
+![](./Screenshot%202023-07-16%20133221.png)
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+require ('dotenv').config();
 const express=require('express')
 const app=express();
-const port=3000
+const port=process.env.PORT
 
 function welcomeHandler(req, res) {
   res.status(200).send("hi from home");
